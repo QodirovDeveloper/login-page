a98b4e412823a52c11cc2016ceae6409a69456a2
QodirovDeveloper <startfly58@gmail.com>
2025-07-27 21:06:26 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/dashboard" element={<Dashboard />} />
